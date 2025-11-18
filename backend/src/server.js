6e79e8bc318c74fe774b24fd290ae39869d8d807
Victorles777 <victorgabrielles000@gmail.com>
2025-11-18 11:22:14 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,6 @@ const app = require("./app");
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-  console.log(`🚀 MindTech IoT API rodando na porta ${PORT}`);
+  console.log(`MindTech IoT API rodando na porta ${PORT}`);
   console.log(`→ POST http://localhost:${PORT}/api/mindtechiot/subscribe`);
 });
